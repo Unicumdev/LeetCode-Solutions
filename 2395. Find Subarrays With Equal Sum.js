a5a80cdfb974c6c005caaea0be8456885ef6382d
Pavel <pavel.paketin@mail.ru>
2024-03-28 16:33:42 +0300

    Create 2395. Find Subarrays With Equal Sum.js
    Given a 0-indexed integer array nums, determine whether there exist two subarrays of length 2 with equal sum. Note that the two subarrays must begin at different indices.
    
    Return true if these subarrays exist, and false otherwise.
    
    A subarray is a contiguous non-empty sequence of elements within an array.
    
     
    
    Example 1:
    
    Input: nums = [4,2,4]
    Output: true
    Explanation: The subarrays with elements [4,2] and [2,4] have the same sum of 6.

---
+++ 2395. Find Subarrays With Equal Sum.js
@@ -0,0 +1,9 @@
+
+var findSubarrays = function(nums) {
+     for (let i = 0; i < nums.length-2; i ++) {
+        for (let j = nums.length-2; j >=i+1; j --) {
+            if (nums[i]+nums[i+1]==nums[j]+nums[j+1]) return true;
+        }
+    }
+return false;
+};
