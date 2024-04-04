59f2eaf469661c0850adb91a939014396189b3e7
Pavel <pavel.paketin@mail.ru>
2024-04-04 16:50:00 +0300

    Create 1995. Count Special Quadruplets.ts
    Given a 0-indexed integer array nums, return the number of distinct quadruplets (a, b, c, d) such that:
    
    nums[a] + nums[b] + nums[c] == nums[d], and
    a < b < c < d
     
    
    Example 1:
    
    Input: nums = [1,2,3,6]
    Output: 1
    Explanation: The only quadruplet that satisfies the requirement is (0, 1, 2, 3) because 1 + 2 + 3 == 6.

---
+++ 1995. Count Special Quadruplets.ts
@@ -0,0 +1,10 @@
+function countQuadruplets(nums: number[]): number {
+let count:number=0
+for (let a=0;a<nums.length-3;a++){
+    for (let b=a+1;b<nums.length-2;b++){
+            for (let c=b+1;c<nums.length-1;c++){
+                            for (let d=c+1;d<nums.length;d++){
+                                if (nums[a] + nums[b] + nums[c] == nums[d]) count++;
+}}}}
+return count;    
+};
