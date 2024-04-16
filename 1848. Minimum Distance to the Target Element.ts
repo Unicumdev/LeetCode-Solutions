f12be511f0b94beb27b74efd73be8ddb986d373e
Pavel <pavel.paketin@mail.ru>
2024-04-16 15:08:33 +0300

    Create 1848. Minimum Distance to the Target Element.ts
    Given an integer array nums (0-indexed) and two integers target and start, find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
    
    Return abs(i - start).
    
    It is guaranteed that target exists in nums.
    
     
    
    Example 1:
    
    Input: nums = [1,2,3,4,5], target = 5, start = 3
    Output: 1
    Explanation: nums[4] = 5 is the only value equal to target, so the answer is abs(4 - 3) = 1.

---
+++ 1848. Minimum Distance to the Target Element.ts
@@ -0,0 +1,7 @@
+function getMinDistance(nums: number[], target: number, start: number): number {
+let arr:number[]=[];
+for (let i=0;i<nums.length;i++){
+    if (nums[i] == target) arr.push(Math.abs(i - start));
+} 
+return Math.min(...arr);     
+};
