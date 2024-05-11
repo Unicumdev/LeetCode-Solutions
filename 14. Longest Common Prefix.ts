d17cb772bf7ded613b60778acd10bfc7be27c500
Pavel <pavel.paketin@mail.ru>
2024-05-11 14:13:09 +0300

    Create 14. Longest Common Prefix.ts
    Write a function to find the longest common prefix string amongst an array of strings.
    
    If there is no common prefix, return an empty string "".
    
     
    
    Example 1:
    
    Input: strs = ["flower","flow","flight"]
    Output: "fl"

---
+++ 14. Longest Common Prefix.ts
@@ -0,0 +1,11 @@
+function longestCommonPrefix(strs: string[]): string {
+    if (strs.length === 0)  return "";
+    let prefix = strs[0];
+    for (let word of strs) {
+        while (word.indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+            if (prefix === '') return ''
+        }
+    }
+    return prefix;  
+};
