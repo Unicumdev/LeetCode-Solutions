73756355c537e996802d4a1c1cdce1bcf56cdefa
Pavel <pavel.paketin@mail.ru>
2024-05-22 15:58:24 +0300

    Create 1346. Check If N and Its Double Exist.js
    Given an array arr of integers, check if there exist two indices i and j such that :
    
    i != j
    0 <= i, j < arr.length
    arr[i] == 2 * arr[j]
     
    
    Example 1:
    
    Input: arr = [10,2,5,3]
    Output: true
    Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]

---
+++ 1346. Check If N and Its Double Exist.js
@@ -0,0 +1,10 @@
+
+var checkIfExist = function(arr) {
+         const n = arr.length;
+        for (let i = 0; i < n; i++) {
+            for (let j = i + 1; j < n; j++) {
+                if (arr[i] * 2 === arr[j] || arr[j] * 2 === arr[i]) return true;
+            }
+        }
+        return false;   
+};
